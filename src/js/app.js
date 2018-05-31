17010d8d077394dcfa860e07721908c1683e82db
zyfneverforget <262829233@qq.com>
2018-05-31 10:58:27 +0800

    webpack-demo

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+ import x from './modules-1'
+ import y from './modules-2'
+ import '../css/main.scss'
+ x()
+ console.log('fuck webpack')
+ y()
+ let a =1 
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist/js/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader"  // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+            options: { importLoaders: 1 }
+          },
+          {
+            loader:  'postcss-loader'
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+    }
+    ]
+  }
+};
